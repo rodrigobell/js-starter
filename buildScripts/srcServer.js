3b7a93230979379f0edee43f4bb41bb0def1849d
Rodrigo Bell <rbmac@Rodrigos-MacBook-Pro.local>
2017-01-12 17:54:00 -0800

    Added Webpack for project bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
 import chalk from 'chalk';
+import webpack from 'webpack';
+import config from '../webpack.config';
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
